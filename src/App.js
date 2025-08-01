2bf206687ef3ee2c7199ffec8b335de14f550166
saloni-1018 <saloniss1020@gmail.com>
2025-08-01 14:33:09 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ import Main from "./containers/Main";
 function App() {
   return (
     <div>
+      {/* New heading for auto-deploy test */}
+      <h1 style={{ textAlign: "center", color: "blue" }}>
+        Hello Sallu! 🚀 Site Updated via CI/CD
+      </h1>
       <Main />
     </div>
   );
